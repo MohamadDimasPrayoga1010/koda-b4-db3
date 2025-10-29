900d83460055a2c14245d546aed8775ca970c373
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-29 21:01:56 +0700

    feat: get actors who have more than 5 roles

--- query3.sql
+++ query3.sql
@@ -6,3 +6,15 @@ SELECT
 FROM directors
 JOIN directors_genres ON directors.id = directors_genres.director_id
 GROUP BY directors.id, directors.first_name, directors.last_name;
+
+
+--mendapatkan actors yang memiliki roles lebih dari 5
+SELECT
+    actors.id AS actor_id,
+    actors.first_name,
+    actors.last_name,
+    COUNT(roles.movie_id) AS role_more_than_5
+FROM actors
+JOIN roles ON actors.id = roles.actor_id
+GROUP BY actors.id, actors.first_name, actors.last_name
+HAVING COUNT(roles.movie_id) > 5;
